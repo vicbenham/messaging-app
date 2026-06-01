e0669f6aa5b147f658f2971e10fc607db005ff08
Victor Benhamou <vbenhamou98@gmail.com>
2026-06-01 12:05:14 +0200

    first commit

---
+++ src/main/java/org/example/messagingapp/MessagingAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.messagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingAppApplication.class, args);
+	}
+
+}
