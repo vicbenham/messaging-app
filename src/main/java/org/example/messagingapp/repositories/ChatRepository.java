6b7cf6042a46af4a54399633db9bc25192199aef
morgane <morgane.bth10@gmail.com>
2026-06-02 16:55:36 +0200

    add ChatRepository and ChatService

--- src/main/java/org/example/messagingapp/repositories/ChatRepository.java
+++ src/main/java/org/example/messagingapp/repositories/ChatRepository.java
@@ -1,9 +1,13 @@
 package org.example.messagingapp.repositories;
 
 import org.example.messagingapp.entities.Chat;
+import org.example.messagingapp.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+    List<Chat> findAllBySenderOrReceiver(Contact sender, Contact receiver);
 }
--- src/main/java/org/example/messagingapp/services/ChatService.java
+++ src/main/java/org/example/messagingapp/services/ChatService.java
@@ -1,13 +1,26 @@
 package org.example.messagingapp.services;
 
+import lombok.AllArgsConstructor;
+import org.example.messagingapp.dtos.ChatView;
 import org.example.messagingapp.entities.Chat;
 import org.example.messagingapp.entities.Contact;
 import org.example.messagingapp.entities.Message;
+import org.example.messagingapp.repositories.ChatRepository;
+import org.example.messagingapp.repositories.ContactRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+@Service
+@AllArgsConstructor
 public class ChatService {
+
+    private final ContactRepository contactRepository;
+    private final ChatRepository chatRepository;
+
     public void send(Message message, UUID chatId){}
 
     public List<Message> getHistory(UUID chatId){
@@ -16,6 +29,24 @@ public class ChatService {
 
     public Chat findOrCreateChat(Contact sender, Contact receiver){
         return null;
+    }
+
+    public List<ChatView> listAllForOneUser(Long userId){
+        Optional<Contact> optionalContact = contactRepository.findById(userId);
+        Contact contact = optionalContact.orElseThrow(() -> new RuntimeException("Contact not found"));
+        List<Chat> chats = chatRepository.findAllBySenderOrReceiver(contact, contact);
+        List<ChatView> results = new ArrayList<>();
+        chats.stream().forEach(chat -> {
+            Contact receiver = chat.getSender().equals(contact.getUsername()) ? chat.getReceiver() : chat.getSender();
+            ChatView chatViewEl = new ChatView(
+                    receiver.getEmail(),
+                    receiver.getUsername(),
+                    chat.getStatus(),
+                    chat.getCreatedAt(),
+                    true);
+            results.add(chatViewEl);
+        });
+        return results;
     }
 
 }
